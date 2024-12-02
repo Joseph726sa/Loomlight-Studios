4bd1b5c56b95d950c551ecc8040fd8b8972a10bd
LeydySenati <1463407@senati.pe>
2024-12-02 14:22:19 -0500

    felicidadesGanaste
    felicidadesGanaste

--- UI.gd
+++ UI.gd
@@ -1,14 +1,17 @@
 extends CanvasLayer
-
+var label: Label
 var contadorLlaves=0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
+	label = $Labelwin
+	label.visible = false
 	$Label_paraContar.text = str(contadorLlaves)
-
+	
 func tomarLlaves():
 	contadorLlaves+=1
 	$Label_paraContar.text = str(contadorLlaves)
 	
 	if contadorLlaves==5:
+		label.visible = true
 		print("ganaste")
